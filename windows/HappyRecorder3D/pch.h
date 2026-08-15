2380d30046a2a4e2efd2f59f81cea151c9522cec
Aldane Hutchinson <adminalchicken@gmail.com>
2026-08-15 04:03:09 +0000

    major updates

--- windows/HappyRecorder3D/pch.h
+++ windows/HappyRecorder3D/pch.h
@@ -21,4 +21,37 @@
 #include <winrt/Microsoft.UI.Xaml.Controls.h>
 #include <winrt/Microsoft.UI.Xaml.Media.h>
 #include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
+
+// Needed by HappyRecorderNativeModule (folded into this project so its
+// REACT_MODULE gets picked up by codegen -- see HappyRecorder3D.vcxproj).
+#include <winrt/Windows.Media.Capture.h>
+#include <winrt/Windows.Media.MediaProperties.h>
+#include <winrt/Windows.Storage.h>
+#include <winrt/Windows.Storage.Streams.h>
+#include <winrt/Windows.Graphics.Display.h>
+#include <winrt/Windows.Devices.Enumeration.h>
+#include <winrt/Windows.Media.Devices.h>
+#include <winrt/Windows.Media.Audio.h>
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.UI.Core.h>
+
+#include <string>
+#include <vector>
+#include <map>
+#include <memory>
+#include <functional>
+#include <chrono>
+#include <thread>
+#include <filesystem>
+
 using namespace winrt::Windows::Foundation;
+using namespace winrt::Windows::System;
+using namespace winrt::Windows::Storage;
+using namespace winrt::Windows::Storage::Streams;
+using namespace winrt::Windows::Devices::Enumeration;
+using namespace winrt::Windows::Media;
+using namespace winrt::Windows::Media::Capture;
+using namespace winrt::Windows::Media::MediaProperties;
+using namespace winrt::Windows::Media::Devices;
+using namespace winrt::Windows::Media::Audio;
+using namespace winrt::Windows::UI::Core;
